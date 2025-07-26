2b8b287a1b605328f42d96cc6ebc7f63b2830344
InfamousFlag98 <guimajr98@gmail.com>
2025-07-26 15:33:36 -0300

    Novo modulo

---
+++ Curso UC Python/uC_Sec3_POO_Python/aula208_209.py
@@ -0,0 +1,52 @@
+"""
+Aula 208 - Convenções de Nomenclatura em Python
+
+Como você viu na aula anterior, usamos certas convenções para nomes de variáveis, funções, classes e assim por diante.
+Essas convenções tem um nome que podemos usar para nos referir ao modo como estamos nomeando determinados objetos em nosso programa: PascalCase, camelCase e snake_case.
+
+PascalCase - significa que todas as palavras iniciam com letra maiúscula e nada é usado para separá-las, como em: MinhaClasse, Classe, MeuObjeto, MeuProgramaMuitoLegal.
+Essa á a convenção utilizada para classes em Python;
+
+camelCase - a única diferença de camelCase para PascalCase é a primeira letra.
+Em camelCase a primeira letra sempre será minúscula e o restante das palavras deverá iniciar com letra maiúscula.
+Como em: minhaFuncao, funcaoDeSoma, etc...
+Essa conversão não é usada em Python (apesar de eu confundir as duas e às vezes acabar chamando camelCase de PascalCase ou vice-versa, mas agora você sabe a diferença);
+
+snake_case - este é o padrão usado em Python para definir qualquer coisa que não for uma classe.
+ Todas as letras serão minúsculas e separadas por um underline, como em: minha_variavel, funcao_legal, soma.
+
+Os padrões usados em Python são: snake_case para qualquer coisa e PascalCase para classes.
+"""
+# Métodos de classe + factories (fábricas)
+# São métodos onde "self" será "cls", ou seja,
+# ao invés de receber a instância no primeiro
+# parâmetro, receberemos a própria classe.
+class Pessoa:
+    ano = 2023  # atributo de classe
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    @classmethod
+    def metodo_de_classe(cls):
+        print('Hey')
+
+    @classmethod
+    def criar_com_50_anos(cls, nome):
+        return cls(nome, 50)
+
+    @classmethod
+    def criar_sem_nome(cls, idade):
+        return cls('Anônima', idade)
+
+
+p1 = Pessoa('João', 34)
+p2 = Pessoa.criar_com_50_anos('Helena')
+p3 = Pessoa('Anônima', 23)
+p4 = Pessoa.criar_sem_nome(25)
+print(p2.nome, p2.idade)
+print(p3.nome, p3.idade)
+print(p4.nome, p4.idade)
+# print(Pessoa.ano)
+# Pessoa.metodo_de_classe()
