14ca410b98d648624814b4e97b07b9b28802b45c
InfamousFlag98 <guimajr98@gmail.com>
2025-08-02 21:59:16 -0300

    x

--- Curso UC Python/uC_Sec3_POO_Python/aula217.py
+++ Curso UC Python/uC_Sec3_POO_Python/aula217.py
@@ -1,39 +1,41 @@
 # Relações entre classes: associação, agregação e composição
-# Associação é um tipo de relação onde os objetos
-# estão ligados dentro do sistema.
-# Essa é a relação mais comum entre objetos e tem subconjuntos
-# como agregação e composição (que veremos depois).
-# Geralmente, temos uma associação quando um objeto tem
-# um atributo que referencia outro objeto.
-# A associação não especifica como um objeto controla
-# o ciclo de vida de outro objeto.
-class Escritor:
-    def __init__(self, nome) -> None:
+# Agregação é uma forma mais especializada de associação
+# entre dois ou mais objetos. Cada objeto terá
+# seu ciclo de vida independente.
+# Geralmente é uma relação de um para muitos, onde um
+# objeto tem um ou muitos objetos.
+# Os objetos podem viver separadamente, mas pode
+# se tratar de uma relação onde um objeto precisa de
+# outro para fazer determinada tarefa.
+# (existem controvérsias sobre as definições de agregação).
+class Carrinho:
+    def __init__(self):
+        self._produtos = []
+
+    def total(self):
+        return sum([p.preco for p in self._produtos])
+
+    def inserir_produtos(self, *produtos):
+        # self._produtos.extend(produtos)
+        # self._produtos += produtos
+        for produto in produtos:
+            self._produtos.append(produto)
+
+    def listar_produtos(self):
+        print()
+        for produto in self._produtos:
+            print(produto.nome, produto.preco)
+        print()
+
+
+class Produto:
+    def __init__(self, nome, preco):
         self.nome = nome
-        self._ferramenta = None
-
-    @property
-    def ferramenta(self):
-        return self._ferramenta
-
-    @ferramenta.setter
-    def ferramenta(self, ferramenta):
-        self._ferramenta = ferramenta
+        self.preco = preco
 
 
-class FerramentaDeEscrever:
-    def __init__(self, nome):
-        self.nome = nome
-
-    def escrever(self):
-        return f'{self.nome} está escrevendo'
-
-
-escritor = Escritor('Luiz')
-caneta = FerramentaDeEscrever('Caneta Bic')
-maquina_de_escrever = FerramentaDeEscrever('Máquina')
-escritor.ferramenta = maquina_de_escrever
-
-print(caneta.escrever())
-print(maquina_de_escrever.escrever())
-print(escritor.ferramenta.escrever())
+carrinho = Carrinho()
+p1, p2 = Produto('Caneta', 1.20), Produto('Camiseta', 20)
+carrinho.inserir_produtos(p1, p2)
+carrinho.listar_produtos()
+print(carrinho.total())
